64436911ff3ccd44fb732e90f5c79b7161d223ca
David Muñoz Lahoz <davidmunozlahoz@gmail.com>
2026-08-06 08:56:53 +0200

    change to nvim

--- config.h
+++ config.h
@@ -219,7 +219,7 @@ static Key keys[] = {
 	{ MODKEY,			XK_b,		spawn,	SHCMD("python /home/david/dev/bookmarks/bookmarks.py") },
 	{ MODKEY|ShiftMask,			XK_b,		togglebar,	{0} },
 	/* { MODKEY|ShiftMask,		XK_b,		spawn,		SHCMD("") }, */
-	{ MODKEY,			XK_n,		spawn,		SHCMD(TERMINAL " -e vim -c VimwikiIndex") },
+	{ MODKEY,			XK_n,		spawn,		SHCMD(TERMINAL " -e nvim -c VimwikiIndex") },
 	{ MODKEY|ShiftMask,		XK_n,		spawn,		SHCMD(TERMINAL " -e newsboat") },
 	{ MODKEY,			XK_m,		spawn,		SHCMD(TERMINAL " -e ncmpcpp") },
 	/* { MODKEY|ShiftMask,		XK_m,		spawn,		SHCMD("pamixer -t; kill -44 $(pidof dwmblocks)") }, */
